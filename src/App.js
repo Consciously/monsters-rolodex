3bc1b40abd4eb4e3ffdbb62db0867008542cb20d
Stefan Ihle <stefanihle@gmx.net>
2022-08-03 15:52:56 +0200

    create search-box componenent and bring neccessary props into it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -40,13 +41,11 @@ class App extends Component {
 		});
 		return (
 			<div className='App'>
-				<input
+				<SearchBox
 					className='search-box'
-					type='search'
-					placeholder='search monsters'
-					onChange={onSearchChange}
+					onChangeHandler={onSearchChange}
+					placeholder='Search monsters...'
 				/>
-
 				<CardList monsters={filteredMonsters} />
 			</div>
 		);
